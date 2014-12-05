8a77afd15f678a980858103c33aab20ed5af787f
lili545442483 <lili545442483@163.com>
2014-12-05 14:13:04 +0800

    add some code

---
+++ vectors.R
@@ -0,0 +1,8 @@
+num_vect <- c(0.5, 55, -10, 6)
+num_vect < 1
+my_char <- c("My", "name", "is")
+paste(my_char, collapse = " ")
+paste("Hello", "world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+
