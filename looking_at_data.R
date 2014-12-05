1be9e925759a11384c82e912cadfd8bf8ab98ace
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-12-05 21:37:01 +0800

    add some code

---
+++ looking_at_data.R
@@ -0,0 +1,13 @@
+ls()
+
+my_data <- matrix(1:10000, 1000, 10)
+
+dim(my_data)
+nrow(my_data)
+ncol(my_data)
+object.size(my_data)
+names(my_data)
+head(my_data, 10)
+tail(my_data, 15)
+summary(my_data)
+str(my_data)
