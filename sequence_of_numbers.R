fc3dbf64ad5ef466dc9d575edcd2b70d01eac4de
lili545442483 <lili545442483@163.com>
2014-12-05 14:02:42 +0800

    add some code

---
+++ sequence_of_numbers.R
@@ -0,0 +1,12 @@
+1 : 10
+pi : 10
+5 : 1
+seq(1, 10)
+seq(0, 10, by = 0.5)
+my_seq <- seq(5, 10, length = 30)
+length(my_seq)
+seq(along.with = my_seq)
+seq_along(my_seq)
+rep(0, times = 40)
+rep(c(0, 1, 2), times = 10)
+rep(c(0, 1, 2), each = 10)
