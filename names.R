e0d76f17fbb245492864325bfcd80cc433aa470f
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-12-06 19:38:34 +0800

    add some code

---
+++ names.R
@@ -0,0 +1,13 @@
+x <- 1:3
+names(x)
+names(x) <- c("foo", "bar", "norf")
+x
+names(x)
+
+x <- list(a = 1, b = 2, c = 3)
+x
+names(x)
+
+m <- matrix(1:4, nrow = 2, ncol = 2)
+dimnames(m) <- list(c("a", "b"), c("c", "d"))
+m
