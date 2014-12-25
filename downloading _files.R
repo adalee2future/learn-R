2355fb4547aa1e3e677b32f8743a06382205c3a3
Ada Lee <adalee2future@users.noreply.github.com>
2014-12-25 08:34:14 +0800

    download and read csv files

--- downloading _files.R
+++ downloading _files.R
@@ -2,4 +2,3 @@ fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessTy
 download.file(fileUrl, destfile = "./data/cameras.csv", method = “curl")
 list.files("./data")
 dataDownloaded <- date()
-
