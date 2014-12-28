906d15caab9204ea9bd466fb8bfefd2c7acb5a51
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-12-28 13:37:14 +0800

    Getting and Cleaning Data

--- merging_data.R
+++ merging_data.R
@@ -19,3 +19,10 @@ head(mergeData)
 df1 = data.frame(id=sample(1:10),x=rnorm(10))
 df2 = data.frame(id=sample(1:10),y=rnorm(10))
 arrange(join(df1,df2),id)
+
+## if you have multiple data frames
+df1 = data.frame(id=sample(1:10),x=rnorm(10))
+df2 = data.frame(id=sample(1:10),y=rnorm(10))
+df3 = data.frame(id=sample(1:10),z=rnorm(10))
+dfList = list(df1,df2,df3)
+join_all(dfList)
