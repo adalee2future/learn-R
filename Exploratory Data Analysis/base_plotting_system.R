0daed3199437de64940ed6d0b99fce97a05e475c
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-12-30 19:57:04 +0800

    add some code

--- Exploratory Data Analysis/base_plotting_system.R
+++ Exploratory Data Analysis/base_plotting_system.R
@@ -1,3 +1,54 @@
 library(datasets)
 ## simple base graphics: histogram
 hist(airquality$Ozone)  ## draw a new plot
+
+## simple base graphics: scatterplot
+with(airquality, plot(Wind, Ozone))
+
+## simple base graphics: boxplot
+airquality <- transform(airquality, Month = factor(Month))
+boxplot(Ozone ~ Month, airquality, xlab = "Month", ylab = "Ozone (ppb)")
+
+## some important base graphics parameters
+## default values for global graphics parameters
+par("lty")
+par("col")
+par("pch")
+par("bg")
+par("mar")
+par("mfrow")
+
+## base plot with annotation
+with(airquality, plot(Wind, Ozone))
+title(main = "Ozone and Wind in New York City")  ## Add a title
+
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City"))
+with(subset(airquality, Month == 5), points(Wind, Ozone, col = "blue"))
+
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City",
+                      type = "n"))
+
+## base plot with regression line
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City",
+                      pch = 20))
+model <- lm(Ozone ~ Wind, airquality)
+abline(model, lwd = 2)
+
+## multiple base plots
+par(mfrow = c(1, 2))
+with(airquality, {
+        plot(Wind, Ozone, main = "Ozone and Wind")
+        plot(Solar.R, Ozone, main = "Ozone and Solar Radiation")
+})
+
+par(mfrow = c(1, 3), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+with(airquality, {
+        plot(Wind, Ozone, main = "Ozone and Wind")
+        plot(Solar.R, Ozone, main = "Ozone and Solar Radiation")
+        plot(Temp, Ozone, main = "Ozone and Temperature")
+        mtext("Ozone and Weather in New York City", outer = TRUE)
+})
+
+
+
+
