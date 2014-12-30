fcfa345a86561ff4c344a668073c025e487ecc8c
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-12-30 21:11:12 +0800

    add some code

---
+++ Exploratory Data Analysis/graphics_devices.R
@@ -0,0 +1,14 @@
+## how does a plot get created?
+
+## the first approach
+library(datasets)
+with(faithful, plot(eruptions, waiting))  ## make plot appear on screen device
+title(main = "Old Faithful Geyser data")  ## annotate with a title
+
+## the second approach
+pdf(file = "myplot.pdf")  ## open PDF device; create 'myplot.pdf' in my working directory
+## create plot and send to a file (no plot appears on screen)
+with(faithful, plot(eruptions, waiting))
+title(main = "Old Faithful Geyser data")  ## annotate plot; still nothing in screen
+dev.off()  ## Close the PDF file device
+## Now you can view the fiel 'myplot.pdf' on your computer
