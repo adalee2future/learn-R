27e305f5f65bb677811c7c2ef32b50f0c924b305
Ada Lee <adalee2future@users.noreply.github.com>
2015-01-24 11:17:22 +0800

    get the js/html or publish

---
+++ Developing Data Products/rCharts_demo4.R
@@ -0,0 +1,8 @@
+require(rCharts)
+r1 <- rPlot(mpg ~ wt | am + vs, data = mtcars, type = "point", color = "gear")
+r1$print("chart1") # print out the js 
+r1$save('myPlot.html') #save as html file
+r1$publish('myPlot', host = 'gist') # save to gist, rjson required
+r1$publish('myPlot', host = 'rpubs') # save to rpubs
+
+
