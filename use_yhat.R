a298bce6348bcfb74728ac744b8a22bd3852c51b
Ada Lee <adalee2future@users.noreply.github.com>
2015-01-28 20:09:40 +0800

    http://cloud.yhathq.com

--- use_yhat.R
+++ use_yhat.R
@@ -1,9 +1,23 @@
-library(yhatr)
+d <- read.csv("annual_all_2013.csv")
+sub <- subset(d, Parameter.Name %in% c("PM2.5 - Local Conditions", "Ozone")
+              & Pullutant.Standard %in% c("Ozone 8-Hour 2008", "PM25 Annual 2006"),
+              c(Longitude, Latitude, Parameter.Name, Arithmetic.Mean))
+pollavg <- aggregate(sub[, "Arithmetic.Mean"], 
+                     sub[, c("Longitude", "Latitude", "Parameter.Name")],
+                     mean, na.rm = TRUE)
+names(pollavg)[4] <- "level"
+pollavg <- transform(pollavg, Parameter.Name = factor(Parameter.Name))
 
-model.require <- function() {
-        library(fields)
-}
+rm(d, sub)
 
+monitors <- data.matrix(pollavg[, c("Longitude", "Latitude")])
+
+library(fields)
+
+## input is data frame with
+## lon: longitude
+## lat: latitude
+## radius: Radius in miles for finding monitors
 pollutant <- function(df) {
         x <- data.matrix(df[, c("lon", "lat")])
         r <- df$radius
@@ -18,10 +32,25 @@ pollutant <- function(df) {
         data.frame(df, dlevels)
 }
 
+library(yhatr)
+
+model.require <- function() {
+        library(fields)
+}
+
 model.transform <- function(df) {
         df
 }
 
-model.predict <- function() {
+model.predict <- function(df) {
         pollutant(df)
 }
+
+yhat.config <- c(username = "lill_10@lzu.edu.cn",
+                 apikey = "6ef041d922786fc4b47bb759e4ca37ae",
+                 env = "http://sandbox.yhathq.com/")
+
+yhat.deploy("pollutant")
+
+
+# {"lon" : -76.61, "lat" : 39.28, "radius" : 40}
