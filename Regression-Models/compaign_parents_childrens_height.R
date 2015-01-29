c086291e9a0b468499560f4810885f12a9ac76d1
Ada Lee <adalee2future@users.noreply.github.com>
2015-01-29 22:22:57 +0800

    Campaign children’s heights and parents’ heights

---
+++ Regression-Models/compaign_parents_childrens_height.R
@@ -0,0 +1,11 @@
+library(UsingR); data(galton)
+
+plot(galton$parent, galton$child, pch = 19, col = "blue")
+
+freqData <- as.data.frame(table(galton$child, galton$parent))
+names(freqData) <- c("child", "parent", "freq")
+plot(as.numeric(as.vector(freqData$parent)), 
+     as.numeric(as.vector(freqData$child)),
+     pch = 21, col = "black", bg = "lightblue",
+     cex = .15 * freqData$freq, 
+     xlab = "parent", ylab = "child")
